ecf717659532f88927f7c7d35818a4718c7dd33d
denysastapov <purple.rain1982@gmail.com>
2026-06-08 13:51:59 -0400

    Register exam models in Django admin

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,65 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Question,
+    Choice,
+    Submission,
+)
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+    extra = 2
 
 
-# Register your models here.
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+    inlines = [LessonInline, QuestionInline]
+    list_display = ["name", "pub_date"]
+    search_fields = ["name", "description"]
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ["title", "course", "order"]
+    list_filter = ["course"]
+    search_fields = ["title", "content"]
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ["content", "course", "grade"]
+    list_filter = ["course"]
+    search_fields = ["content"]
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["content", "question", "is_correct"]
+    list_filter = ["is_correct", "question"]
+    search_fields = ["content"]
+
+
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ["id", "enrollment"]
+    filter_horizontal = ["choices"]
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Submission, SubmissionAdmin)
